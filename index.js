7aa0c5dc3bd6de8814890086a1e4f2e88b0b89c4
Jack Pritchard <jack@noface.co.uk>
2019-04-08 19:17:47 +0100

    Remove weird async method things

--- index.js
+++ index.js
@@ -10,43 +10,37 @@ if (!fs.existsSync(dateStamp)) {
 }
 
 async function doScreenCapture(url, site_name, device) {
-	try {
-		const browser = await puppeteer.launch();
-		const page = await browser.newPage();
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
 
-		const deviceViewport = {
-			height: device.height,
-			width: device.width
-		};
+	const deviceViewport = {
+		height: device.height,
+		width: device.width
+	};
 
-		await page.setViewport(deviceViewport);
-		await page.goto(url, { waitUntil: `networkidle0` });
+	await page.setViewport(deviceViewport);
+	await page.goto(url, { waitUntil: `networkidle0` });
 
-		const bodyHandle = await page.$(`body`);
-		const { width, height } = deviceViewport;
+	const bodyHandle = await page.$(`body`);
+	const { width, height } = deviceViewport;
 
-		const screenshot = await page.screenshot({
-			clip: {
-				x: 0,
-				y: 0,
-				width,
-				height
-			},
-			path: `${dateStamp}/${device.size}-${site_name}.png`
-		});
+	const screenshot = await page.screenshot({
+		clip: {
+			x: 0,
+			y: 0,
+			width,
+			height
+		},
+		path: `${dateStamp}/${device.size}-${site_name}.png`
+	});
 
-		await browser.close();
-	} catch (e) {
-		console.error(`Error in capturing site for ${site_name}`, e);
-	}
+	await browser.close();
 }
 
 sites.map(site => {
 	try {
 		breakpoints.map(device => {
-			setTimeout(() => {
-				doScreenCapture(site.url, site.name, device);
-			}, 10000);
+			doScreenCapture(site.url, site.name, device);
 		});
 	} catch (e) {
 		console.error(`Error in capturing site for ${site.name}`, e);
